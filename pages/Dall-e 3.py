1da20301b5432eacb96b962cd92c6ac703fa7445
Suhaylito <suhayl.balladin@eemi.com>
2024-09-26 15:25:50 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title("Dalle-3")
 
-st.write ("Ceci est un formulaire de contact")
+st.write ("veuillez rentrer l")
+
+user_input = st.text_input("Tapez votre texte : ")
+
+st.write (user_input)
