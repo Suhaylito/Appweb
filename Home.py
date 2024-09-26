274d2f2de9445afa9bc877915d3f47c5413ebbbe
Suhaylito <suhayl.balladin@eemi.com>
2024-09-26 14:22:40 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,4 +13,4 @@ st.write (user_input)
 
 
 #image 
-St.image("https://www.radiofrance.fr/s3/cruiser-production/2021/09/6be9e0c8-44b5-4b1a-b7a1-60f65971301d/1200x680_pop-smoke.jpg")
+st.image("https://www.radiofrance.fr/s3/cruiser-production/2021/09/6be9e0c8-44b5-4b1a-b7a1-60f65971301d/1200x680_pop-smoke.jpg")
