f1f4f88a93a5fc6773beb596c6704737e60c7f39
Suhaylito <suhayl.balladin@eemi.com>
2024-09-26 14:28:03 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,3 +14,6 @@ st.write (user_input)
 
 #image 
 st.image("https://www.radiofrance.fr/s3/cruiser-production/2021/09/6be9e0c8-44b5-4b1a-b7a1-60f65971301d/1200x680_pop-smoke.jpg")
+
+#Sidebar
+st.sidebar.title ("Balladin Suhayl")
