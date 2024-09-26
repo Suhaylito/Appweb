902d1bba7584e08fac0d235d1220f0c17eae3bb7
Suhaylito <suhayl.balladin@eemi.com>
2024-09-26 14:11:50 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-
+# Titre
 st.title(" Mon formulaire")
+
+# Texte
+st.write ("Ceci est un formulaire de contact")
+
+#Champ de saisi
+st.input_text ("Tapez votre texte :")
