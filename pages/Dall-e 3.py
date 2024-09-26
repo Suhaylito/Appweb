7affb04726d952fa00803fb439d5c242e8f3a79e
Suhaylito <suhayl.balladin@eemi.com>
2024-09-26 15:27:30 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 st.title("Dalle-3")
 
-st.write ("veuillez rentrer l")
-
-user_input = st.text_input("Tapez votre texte : ")
+st.write ("Générer une image facilement juste ici : ")
 
 st.write (user_input)
