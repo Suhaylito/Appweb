ec63bf7340a464a1c8499bcc2c527a33ef9495f8
Suhaylito <suhayl.balladin@eemi.com>
2024-09-26 14:38:01 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,5 +19,10 @@ st.image("https://www.radiofrance.fr/s3/cruiser-production/2021/09/6be9e0c8-44b5
 st.sidebar.title ("Balladin Suhayl")
 
 #video dans la sidebar
-
 st.sidebar.video ("https://www.youtube.com/watch?v=eW_TvHbnHwA")
+
+#Select bare
+st.selectbox ("Selectionnez votre niveau d'étude",["BAC", "BAC +2", "BAC +3"])
+
+#Select slider
+age = st.select_slider ("Quel est votre âge ? ", range(0,99))
